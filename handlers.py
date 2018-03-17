68cc66989483e6c6b3b67ceec1dfc0417783ae23
gleb <gleb.makarchuk@gmail.com>
2018-03-17 16:57:57 +0300

    Added user filtering and implemented async message posting for long executions

--- handlers.py
+++ handlers.py
@@ -1,13 +1,15 @@
 from key_gen import get_public_save_private_key
 from time import sleep
+from collections import deque
 import paramiko
 import threading
 
 FILE_SECRET = 'private.key'
 LINES_PER_MSG = 10
 
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='still under development')
+    bot.send_message(chat_id=update.message.chat_id, text='Hello.')
 
 
 def new_key(bot, update):
@@ -15,49 +17,47 @@ def new_key(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=public.decode('utf-8'))
 
 
-# class Buffer:
-#     def __init__(self, stdout, bot, timeout=.25):
-#         self.bot = bot
-#         self._buffer = []
-#         self.timeout=timeout
-#
-#
-#     t = threading.Thread(target=task, args=(data,))
-#     t.start()
-#
-#     def cycle_post(self):
-#         while True:
-#             if len(self._buffer) != 0:
-#                 self.bot.send_message(chat_id=update.message.chat_id)
-#
-#             if len(self._buffer) == 0:
-#                 return
-#
-#     def flush(self):
-#         self.bot.send_message(chat_id)
+class Buffer:
+    def __init__(self, bot, chat_id, timeout=.2):
+        self.bot = bot
+        self.chat_id = chat_id
+        self._buffer = deque()
+        self.timeout = timeout
+        self._closed = False
+        self.thread = threading.Thread(target=self._start)
+        self.thread.start()
+
+    def append(self, line):
+        self._buffer.append(line)
+
+    def close(self):
+        self._closed = True
+        self.thread.join()
+
+    def _start(self):
+        # runs in a separate thread
+        while True:
+            self._send_buffer()
+            if self._closed:
+                return
+            sleep(self.timeout)
+
+    def _send_buffer(self):
+        lines = []
+        while len(self._buffer) > 0:
+            lines.append(self._buffer.popleft())
+        if len(lines) > 0:
+            self.bot.send_message(chat_id=self.chat_id, text=''.join(lines))
 
 
 def bash(bot, update):
     client = get_client()
-    stdin, stdout, stderr = client.exec_command(update.message.text, get_pty=True)
-    print(type(stdout))
-    # lines = stdout.readlines()
-    # bot.send_message(chat_id=update.message.chat_id, text=''.join(lines))
-
-    print('chat_id', update.message.chat_id)
-
-    buffer = []
+    _, stdout, _ = client.exec_command(update.message.text, get_pty=True)
+    buffer = Buffer(bot, update.message.chat_id)
     for i, line in enumerate(iter(stdout.readline, '')):
         buffer.append(line)
-        if len(buffer) == LINES_PER_MSG:
-            bot.send_message(chat_id=update.message.chat_id, text=''.join)
-        print('iteration')
-        bot.send_message(chat_id=update.message.chat_id, text=''.join(lines))
-        sleep(0.25)
-    print('here')
-    print(update.message.chat_id)
+    buffer.close()
     bot.send_message(chat_id=update.message.chat_id, text='### finished')
-    print('here2')
     client.close()
 
 
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, DispatcherHandlerStop
 from handlers import new_key, start, bash
 
 token_path = '/Users/gleb/secret/tg_bot1_token'
+username = 'glebmakarchuk'
 
 with open(token_path, 'r') as f:
     token = f.read().strip()
@@ -13,13 +14,18 @@ print(bot.get_me())
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
 
-new_key_handler = CommandHandler('newkey', new_key)
-dispatcher.add_handler(new_key_handler)
+def check_user(_, update):
+    if update.message.from_user.username != username:
+        raise DispatcherHandlerStop
 
-bash_handler = MessageHandler(Filters.text, bash)
-dispatcher.add_handler(bash_handler)
+
+dispatcher.add_handler(MessageHandler(Filters.all, check_user), group=-1)
+
+dispatcher.add_handler(CommandHandler('start', start))
+
+dispatcher.add_handler(CommandHandler('newkey', new_key))
+
+dispatcher.add_handler(MessageHandler(Filters.text, bash))
 
 updater.start_polling()

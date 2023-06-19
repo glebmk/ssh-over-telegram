43859345070824e853ee4b8938f2c2961fb58469
tch1001 <tanchienhao@gmail.com>
2023-06-20 00:09:48 +0800

    fixed send_message errors

--- handlers.py
+++ handlers.py
@@ -9,22 +9,22 @@ import logging
 logger = logging.getLogger()
 
 
-def start(bot, update):
+def start(update, context):
     logger.info('Received start command')
     text = "Hello. If you are new here or want to change your ssh key pair, run /newkey. " \
            "Please note that this command will overwrite old private key."
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+    update.message.reply_text(text=text)
 
 
-def new_key(bot, update):
+def new_key(update, context):
     logger.info('Received newkey command')
     public = get_public_save_private_key()
-    bot.send_message(chat_id=update.message.chat_id, text=public.decode('utf-8'))
+    update.message.reply_text(text=public.decode('utf-8'))
     text = "You have just received the public key. " \
            "Private key was stored in the directory from which the bot is running. " \
            "Please, add it to the authorized keys on the server. " \
            "This can be done, by appending public key to server's authorized_keys file (~/.ssh/authorized_keys)."
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+    update.message.reply_text(text=text)
 
 
 class Buffer:
@@ -57,37 +57,36 @@ class Buffer:
         while len(self._buffer) > 0:
             lines.append(self._buffer.popleft())
         if len(lines) > 0:
-            self.bot.send_message(chat_id=self.chat_id, text=''.join(lines))
+            self.bot.message.reply_text(text=''.join(lines))
 
 
 @run_async
-def cancel_signal(bot, update, client_holder, connection_info):
+def cancel_signal(update, context, client_holder, connection_info):
     logger.info('Received cancel signal command')
-    if client_holder_is_bad(bot, update, client_holder, connection_info):
+    if client_holder_is_bad(update, context, client_holder, connection_info):
         return
     client_holder[0].close()
     client_holder[0] = get_client(connection_info)
-    bot.send_message(chat_id=update.message.chat_id, text='### connection was reestablished')
+    update.message.reply_text(text='### connection was reestablished')
 
 
-@run_async
-def shell(bot, update, client_holder, connection_info):
+def shell(update, context, client_holder, connection_info):
     logger.info(f'Received shell command: {update.message.text}')
-    if client_holder_is_bad(bot, update, client_holder, connection_info):
+    if client_holder_is_bad(update, context, client_holder, connection_info):
         return
     _, stdout, _ = client_holder[0].exec_command(update.message.text, get_pty=True)
-    buffer = Buffer(bot, update.message.chat_id)
+    buffer = Buffer(update, update.message.chat_id)
     for i, line in enumerate(iter(stdout.readline, '')):
         buffer.append(line)
     buffer.close()
-    bot.send_message(chat_id=update.message.chat_id, text='### finished')
+    update.message.reply_text(text='### finished')
 
 
-def client_holder_is_bad(bot, update, client_holder, connection_info):
+def client_holder_is_bad(update, context, client_holder, connection_info):
     if client_holder[0] is None:
         client_holder[0] = get_client(connection_info)
     if client_holder[0] is None:
         logger.warning('Some problem with connection')
-        bot.send_message(chat_id=update.message.chat_id, text='### some problem with the connection')
+        update.message.reply_text(text='### some problem with the connection')
         return True
     return False
